e3098af74fbaafced4226655ba6beef51ae94d1d
Aliaksei Kazlou <akazlou2@exadel.com>
2018-07-31 18:36:16 +0300

    Update UI.

--- Kpd37Gomel/Kpd37Gomel/ClientAppNew/src/app/app.module.ts
+++ Kpd37Gomel/Kpd37Gomel/ClientAppNew/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
+import { ButtonsComponent } from './shared/layout/header/buttons/buttons.component';
+import { CollapseComponent } from './shared/layout/header/collapse/collapse.component';
+import { SearchComponent } from './shared/layout/header/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    ButtonsComponent,
+    CollapseComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
